8ef6f17cf3aeb7b15fbe9b4959e9765a2bc89fda
Victor Varela <varelavv@outlook.es>
2025-11-21 23:53:26 +0100

    run build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: './',
-    server: {
-        watch: {
-            usePolling: true,
-        }
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, '/index.html'),
+        ejercicio1: resolve(__dirname, '/ejercicio1.html'),
+        ejercicio2: resolve(__dirname, '/ejercicio2.html'),
+        ejercicio3: resolve(__dirname, '/ejercicio3.html'),
+        ejercicio4: resolve(__dirname, '/ejercicio4.html'),
+        ejercicio5: resolve(__dirname, '/ejercicio5.html'),
+        ejercicio6: resolve(__dirname, '/ejercicio6.html'),
+        ejercicio7: resolve(__dirname, '/ejercicio7.html'),
+        ejercicio8: resolve(__dirname, '/ejercicio8.html'),
+        ejercicio9: resolve(__dirname, '/ejercicio9.html'),
+      }
     }
+  }
 });
